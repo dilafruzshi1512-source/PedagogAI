da0433316b6d6dcedeef38203164aeb3756eb4ea
dilafruzshi1512-source <dilafruzshi1512@gmail.com>
2026-06-02 02:47:20 +0500

    Delete src directory

--- src/api/chat.js
+++
@@ -1,9 +0,0 @@
-import { apiRequest } from './client'
-
-export function sendChatMessage(message, token) {
-  return apiRequest('/chat', {
-    method: 'POST',
-    body: { message },
-    token,
-  })
-}
