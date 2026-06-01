a74b48a912c2a0a97cdbfcf18d00c112e61c2bc4
dilafruzshi1512-source <dilafruzshi1512@gmail.com>
2026-06-02 03:39:12 +0500

    Update main.py

--- main.py
+++ main.py
@@ -45,10 +45,12 @@ app.add_middleware(
         "http://127.0.0.1:5173",
         "http://localhost:4173",
         "http://127.0.0.1:4173",
+        "https://pedagog-ai.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
+
 )
 
 
